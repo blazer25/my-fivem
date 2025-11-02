e57e02bca6e9dcadaac975e05fd881fc1c2b2c5e
cstone1995 <xdreamzclan@gmail.com>
2025-11-02 23:20:36 +0000

    d

--- resources/fivem/Qbox_052EBB.base/resources/[cars]/cfx-stxh-jakers/fxmanifest.lua
+++ resources/fivem/Qbox_052EBB.base/resources/[cars]/cfx-stxh-jakers/fxmanifest.lua
@@ -1,19 +1,15 @@
 fx_version 'cerulean'
-
 game 'gta5'
 
 author 'Jakers'
-
 description 'stxh'
-
-version '1'
+version '1.0.0'
 
 files {
-
     'vehicles.meta',
     'carvariations.meta',
     'carcols.meta',
-    'handling.meta',
+    'handling.meta'
 }
 
 data_file 'HANDLING_FILE' 'handling.meta'
@@ -21,9 +17,12 @@ data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
 data_file 'CARCOLS_FILE' 'carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
 
+-- Optional: if your model includes a vehiclelayouts.meta
+-- data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
 
+-- Unencrypted textures (skip for escrow)
 escrow_ignore {
-    'stream/**/*.ytd',
+    'stream/**/*.ytd'
 }
 
 lua54 'yes'
