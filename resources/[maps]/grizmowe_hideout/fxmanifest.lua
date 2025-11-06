a3dc19ad47a801fe999564899e1c249e835492c0
cstone1995 <xdreamzclan@gmail.com>
2025-11-06 21:31:44 +0000

    g

--- resources/[maps]/grizmowe_hideout/fxmanifest.lua
+++ resources/[maps]/grizmowe_hideout/fxmanifest.lua
@@ -1,9 +1,9 @@
 fx_version 'cerulean'
-games { 'gta5' }
+game 'gta5'
 
 this_is_a_map 'yes'
 
-author'Grizmowe'
+author 'Grizmowe'
 
 data_file 'DLC_ITYP_REQUEST' 'stream/griz_egout_porte'
 data_file 'DLC_ITYP_REQUEST' 'stream/v_int_74'
@@ -14,6 +14,5 @@ data_file 'DLC_ITYP_REQUEST' 'stream/griz_egout_decal_0'
 data_file 'DLC_ITYP_REQUEST' 'stream/aletomax_fuomogeno_wna'
 data_file 'DLC_ITYP_REQUEST' 'stream/griz_egout_col'
 data_file 'DLC_ITYP_REQUEST' 'stream/griz_egout_fenetre'
-
 
 dependency '/assetpacks'
--- resources/[maps]/new_map/fxmanifest.lua
+++ resources/[maps]/new_map/fxmanifest.lua
@@ -2,4 +2,3 @@ fx_version 'cerulean'
 game 'gta5'
 
 this_is_a_map 'yes'
-
