337e0c57b893369c8d8c14739c7dd3f9ae5b82d0
cstone1995 <xdreamzclan@gmail.com>
2025-11-06 19:49:46 +0000

    d

---
+++ resources/[standalone]/DumpsterDiving/DumpsterDiving/Install/DumpsterDiving/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Mints Modifications'
+description 'Standalone Dumpster Diving Script - Expanded Version'
+version '2.0.0'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+dependency '/assetpacks'
