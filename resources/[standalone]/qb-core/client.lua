0898379853b6ad6e9378d46b02afa41239d4d735
cstone1995 <xdreamzclan@gmail.com>
2025-11-07 22:39:38 +0000

    d

--- resources/[standalone]/qb-core/client.lua
+++ resources/[standalone]/qb-core/client.lua
@@ -1,11 +1,12 @@
--- Simple QBCore bridge for Qbox
 CreateThread(function()
     while not GetResourceState('qbx_core'):find('start') do
         Wait(50)
     end
+
     local QBCore = exports['qbx_core']:GetCoreObject()
     exports('GetCoreObject', function()
         return QBCore
     end)
+
     print('[qb-core] Bridge client initialized.')
 end)
--- resources/[standalone]/qb-core/server.lua
+++ resources/[standalone]/qb-core/server.lua
@@ -1,11 +1,35 @@
--- Simple QBCore bridge for Qbox
 CreateThread(function()
     while not GetResourceState('qbx_core'):find('start') do
         Wait(50)
     end
+
     local QBCore = exports['qbx_core']:GetCoreObject()
+
+    -- Basic QBCore export
     exports('GetCoreObject', function()
         return QBCore
     end)
-    print('[qb-core] Bridge server initialized.')
+
+    -- Compatibility with older QB scripts:
+    exports('GetPlayers', function()
+        return QBCore.Functions.GetPlayers()
+    end)
+
+    exports('GetQBPlayers', function()
+        return QBCore.Functions.GetQBPlayers()
+    end)
+
+    exports('GetPlayer', function(source)
+        return QBCore.Functions.GetPlayer(source)
+    end)
+
+    exports('GetPlayerByCitizenId', function(citizenId)
+        return QBCore.Functions.GetPlayerByCitizenId(citizenId)
+    end)
+
+    exports('GetPlayerByPhone', function(phoneNumber)
+        return QBCore.Functions.GetPlayerByPhone(phoneNumber)
+    end)
+
+    print('[qb-core] Bridge server initialized with legacy exports.')
 end)
