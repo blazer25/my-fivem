12c5e66d75dc3d3627acc5521ee226ed1a08073a
cstone1995 <xdreamzclan@gmail.com>
2025-11-09 23:07:10 +0000

    Update client_config.lua

--- resources/jpr-phonesystem/configs/client_config.lua
+++ resources/jpr-phonesystem/configs/client_config.lua
@@ -1,4 +1,18 @@
-local QBCore                  = exports[Config.CoreName]:GetCoreObject()
+-- Framework detection bridge for Qbox and QB-Core
+
+local Framework = nil
+
+if GetResourceState('qbx_core') == 'started' then
+    -- Use Qbox's global core (does not need export)
+    Framework = exports['qbx_core'] and exports['qbx_core']:GetCoreObject and exports['qbx_core']:GetCoreObject() or QBX
+elseif GetResourceState('qb-core') == 'started' then
+    Framework = exports['qb-core']:GetCoreObject()
+else
+    print('^1[JPR Phone] No valid framework found (qbx_core or qb-core). Using fallback mode.^0')
+    Framework = {}
+end
+
+return Framework
 
 function VehicleKeys(plate, vehicle)
     Citizen.Wait(1000) -- leave this
