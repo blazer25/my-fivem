ba688cae90bb712eff9c27f52e2cc1d2b5dbe63f
cstone1995 <xdreamzclan@gmail.com>
2025-11-09 22:00:47 +0000

    s

--- resources/[standalone]/G9-cleanerJob/html/locale.js
+++
@@ -1,9 +0,0 @@
-function setLang(data) {
-    $('.header .headerText').html(data.header)
-    $('.level .textArea').html(data.headerComment)
-    $('.job .text').html(data.streetComment)
-    $('#garden .text').html(data.gardenComment)
-    $('.start').html(data.startJob)
-    $('.job .img p').html(data.streetName)
-    $('#garden .img p').html(data.gardenName)
-}
