a7b5072f456203849982d499700192a4c0ebc68f
cstone1995 <xdreamzclan@gmail.com>
2025-11-09 16:13:53 +0000

    Update client.lua

--- resources/[qbx]/qb-target/client.lua
+++ resources/[qbx]/qb-target/client.lua
@@ -23,6 +23,8 @@ local HasEntityClearLosToEntity = HasEntityClearLosToEntity
 local currentResourceName = GetCurrentResourceName()
 local Config, Types, Players, Entities, Models, Zones, nuiData, sendData, sendDistance = Config, { {}, {}, {} }, {}, {},
 	{}, {}, {}, {}, {}
+Config = Config or {}
+Config.HideEye = true
 local playerPed, targetActive, hasFocus, success, pedsReady, allowTarget = PlayerPedId(), false, false, false, false,
 	true
 local screen = {}
@@ -91,6 +93,7 @@ local function DrawTarget()
 				end
 
 				SetDrawOrigin(zone.center.x, zone.center.y, zone.center.z, 0)
+				if not Config.HideEye then 
 				DrawSprite('shared', 'emptydot_32', 0, 0, 0.01, 0.02, 0, r, g, b, a)
 				ClearDrawOrigin()
 			end
