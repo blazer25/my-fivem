06bd1866a8bd2f76ffd89ab7b28c91884e5d45c8
cstone1995 <xdreamzclan@gmail.com>
2025-11-09 23:20:18 +0000

    s

--- resources/jpr-phonesystem/configs/get_framework_config.lua
+++ resources/jpr-phonesystem/configs/get_framework_config.lua
@@ -1,21 +1,20 @@
 -- Framework detection bridge for Qbox / QB-Core
-
 local Framework = nil
 
--- Check which framework is running
 if GetResourceState('qbx_core') == 'started' then
-    -- Qbox: It doesn't use the export GetCoreObject, so fallback to global QBX if available
-    if exports['qbx_core'] and exports['qbx_core'].GetCoreObject then
-        Framework = exports['qbx_core']:GetCoreObject()
-    elseif QBX then
+    if QBX then
         Framework = QBX
+        print("^2[JPR Phone] Linked to QBOX Core (qbx_core)^0")
     else
-        print("^1[JPR Phone] qbx_core is started, but no usable core object found!^0")
+        print("^1[JPR Phone] qbx_core started but QBX global not found!^0")
+        Framework = {}
     end
 elseif GetResourceState('qb-core') == 'started' then
     Framework = exports['qb-core']:GetCoreObject()
+    print("^2[JPR Phone] Linked to QB-Core^0")
 else
-    print("^1[JPR Phone] No framework detected (qbx_core or qb-core)^0")
+    print("^1[JPR Phone] No framework found (qbx_core or qb-core)^0")
+    Framework = {}
 end
 
 return Framework
