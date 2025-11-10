1846a37dfd4f4536c7c85f02e60cadbe0e6a0fd4
cstone1995 <xdreamzclan@gmail.com>
2025-11-10 16:38:37 +0000

    s

--- resources/[maps]/jpr-casinosystem/configs/general/get_core_export.lua
+++ resources/[maps]/jpr-casinosystem/configs/general/get_core_export.lua
@@ -1,14 +1,16 @@
-local QBCore = nil
+local Core = nil
 
 if GetResourceState('qbx_core') == 'started' then
-    QBCore = QBX           -- ✅ Global variable (Qbox)
+    Core = QBX
+    QBCore = QBX    -- ✅ backward-compatibility alias
     print('^2[JPR Casino] Linked to QBOX Core via global QBX^0')
 elseif GetResourceState('qb-core') == 'started' then
-    QBCore = exports['qb-core']:GetCoreObject()
+    Core = exports['qb-core']:GetCoreObject()
+    QBCore = Core
     print('^2[JPR Casino] Linked to QB-Core^0')
 else
     print('^1[JPR Casino] No core framework detected (qbx_core or qb-core)^0')
-    QBCore = {}
+    Core, QBCore = {}, {}
 end
 
-return QBCore
+return Core
