adb8a6b87c65052681f006c42af71c8fded44457
cstone1995 <xdreamzclan@gmail.com>
2025-11-10 15:32:38 +0000

    s

--- resources/[maps]/jpr-casinosystem/configs/general/get_core_export.lua
+++ resources/[maps]/jpr-casinosystem/configs/general/get_core_export.lua
@@ -1,2 +1,15 @@
-QBCore = exports[Config.CoreName]:GetCoreObject()
-TargetZonesCreated = {}
+-- QBOX / QB-Core Auto Framework Bridge
+local QBCore = nil
+
+if GetResourceState('qbx_core') == 'started' then
+    QBCore = exports['qbx_core']:GetCoreObject()
+    print('^2[JPR Casino] Linked to QBOX Core (qbx_core)^0')
+elseif GetResourceState('qb-core') == 'started' then
+    QBCore = exports['qb-core']:GetCoreObject()
+    print('^2[JPR Casino] Linked to QB-Core^0')
+else
+    print('^1[JPR Casino] No Core Framework Found (qbx_core or qb-core)^0')
+    QBCore = {}
+end
+
+return QBCore
