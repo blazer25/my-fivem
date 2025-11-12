c529d52de830b51c59ad62c3218f1fa550a7a418
cstone1995 <xdreamzclan@gmail.com>
2025-11-12 13:46:59 +0000

    f

--- resources/[ox]/ox_inventory/data/items.lua
+++ resources/[ox]/ox_inventory/data/items.lua
@@ -869,4 +869,15 @@ return {
             export = 'chris_businesses.useBusinessLaptop'
         }
     },
+    ['rentalpapers'] = {
+        label = "Rental Papers",
+        weight = 0,
+        stack = false,
+        close = false,
+        description = "Rental Papers",
+        client = {
+            image = "rentalpapers.png",
+        },
+    },
 }
+
