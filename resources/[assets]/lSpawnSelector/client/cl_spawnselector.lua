b23091e8181a5148f7faf15f8d1c1d84b4e92ce4
cstone1995 <xdreamzclan@gmail.com>
2025-11-12 17:19:22 +0000

    gt

--- resources/[assets]/lSpawnSelector/client/cl_spawnselector.lua
+++ resources/[assets]/lSpawnSelector/client/cl_spawnselector.lua
@@ -1,6 +1,45 @@
 BOUTIQUE = BOUTIQUE or {}
 BOUTIQUE.Config = BOUTIQUE.Config or {}
 
+local function getFallbackSpawn()
+    local index = BOUTIQUE.Config.fallbackSpawnIndex or 1
+    return BOUTIQUE.Config.spawns[index]
+end
+
+local function getLastLocation()
+    local playerData = exports['qbx_core'] and exports['qbx_core']:GetPlayerData() or nil
+    if not playerData then return nil end
+
+    local position = playerData.position or playerData.lastLocation
+    if not position or not position.x or not position.y or not position.z then return nil end
+
+    local heading = position.w or position.heading or position.h or 0.0
+    return vector4(position.x, position.y, position.z, heading)
+end
+
+local function notify(message, notifyType)
+    if lib and lib.notify then
+        lib.notify({
+            title = BOUTIQUE.Config.title or 'Spawn',
+            description = message,
+            type = notifyType or 'inform'
+        })
+    else
+        TriggerEvent('QBCore:Notify', message, notifyType or 'inform')
+    end
+end
+
+local function spawnPlayerAt(position, ped)
+    local pPed = ped or PlayerPedId()
+    TriggerEvent("CORE.UI:Close")
+    TeleportToWp(pPed, position, position.w)
+    Wait(1000)
+    TriggerServerEvent('QBCore:Server:OnPlayerLoaded')
+    TriggerEvent('QBCore:Client:OnPlayerLoaded')
+    TriggerServerEvent('qb-houses:server:SetInsideMeta', 0, false)
+    TriggerServerEvent('qb-apartments:server:SetInsideMeta', 0, 0, false)
+end
+
 AddEventHandler("spawnselector:open", function()
     OpenUI(true, "spawnselector", {}, {
         title = BOUTIQUE.Config.title,
@@ -13,20 +52,30 @@ AddEventHandler("spawnselector:open", function()
 end)
 
 RegisterNetEvent('CORE.UI:spawnselector:spawn', function(data)
-    local pPed = PlayerPedId()
-
     if not data then return end
-    local spawn = data.spawn
-    if not spawn then return end
-    spawn = BOUTIQUE.Config.spawns[spawn + 1]
 
-    if spawn.position then
-        TriggerEvent("CORE.UI:Close")
-        TeleportToWp(pPed, spawn.position, spawn.position.w)
-        Wait(1000)
-        TriggerServerEvent('QBCore:Server:OnPlayerLoaded')
-        TriggerEvent('QBCore:Client:OnPlayerLoaded')
-        TriggerServerEvent('qb-houses:server:SetInsideMeta', 0, false)
-        TriggerServerEvent('qb-apartments:server:SetInsideMeta', 0, 0, false)
+    local spawnIndex = data.spawn
+    if spawnIndex == nil then return end
+
+    local selection = BOUTIQUE.Config.spawns[spawnIndex + 1]
+    if not selection then return end
+
+    local ped = PlayerPedId()
+
+    if selection.useLastLocation then
+        local lastLocation = getLastLocation()
+        if lastLocation then
+            spawnPlayerAt(lastLocation, ped)
+            return
+        else
+            notify('No previous location found. Spawning you at the default location.', 'error')
+            selection = getFallbackSpawn()
+        end
+    end
+
+    if selection and selection.position then
+        spawnPlayerAt(selection.position, ped)
+    else
+        notify('Unable to determine a spawn location. Please try again in a moment.', 'error')
     end
 end)
--- resources/[assets]/lSpawnSelector/shared/sh_spawnselector.lua
+++ resources/[assets]/lSpawnSelector/shared/sh_spawnselector.lua
@@ -6,12 +6,23 @@ BOUTIQUE.Config = {
     subtitle = "Selector",
     serverLogo = "https://cdn.discordapp.com/attachments/885459782100738071/1370799137959968828/logopng.png?ex=6820cfae&is=681f7e2e&hm=88640bcb7316cbbd613a002ebbbf7141b433e036aff609d94dac3fbb0127d009&",
     color = '#FF5B5B',
+    fallbackSpawnIndex = 2,
     spawns = {
         {
+            name = "Last Location",
+            adress = "Where you logged out",
+            smalldescription = "Resume from your previous spot",
+            description = "Spawn exactly where you left off the last time you were in the city.",
+            image = "https://images.hdqwalls.com/wallpapers/gta-v-city-lights-4k-r0.jpg",
+            top = 10,
+            left = 50,
+            useLastLocation = true,
+        },
+        {
             name = "Airport",
-            adress = "Aéroport de Los Santos",
-            smalldescription = "Aéroport international de Los Santos",
-            description = "Atterrissez sur l'aéroport de Los Santos pour commencer votre aventure dans le luxe et le confort.",
+            adress = "Los Santos International Airport",
+            smalldescription = "Arrivals terminal at LSIA",
+            description = "Touch down at Los Santos International Airport and start your journey in the city with easy access to transport.",
             image = "https://img.gta5-mods.com/q85-w800/images/airport-police-station/777759-LSIA-GTAV-exterior1.jpg",
             top = 69,
             left = 86,
@@ -19,9 +30,9 @@ BOUTIQUE.Config = {
         },
         {
             name = "Paleto Bay",
-            adress = "Motel Paleto Bay",
-            smalldescription = "Reception du motel Paleto Bay",
-            description = "Un motel avec vue panoramique sur Paleto Bay, idéal pour commencer votre aventure dans le luxe et le confort.",
+            adress = "Paleto Bay Motel",
+            smalldescription = "Paleto Bay motel reception",
+            description = "Wake up to ocean views in Paleto Bay and enjoy a quieter start to your day on the north coast.",
             image = "https://img.gta5-mods.com/q75/images/paleto-bay-station-bus/a140e5-GTA5%202015-11-03%2016-14-17-52.jpg",
             top = 55,
             left = 18,
@@ -29,9 +40,9 @@ BOUTIQUE.Config = {
         },
         {
             name = "Sandy Shores",
-            adress = "Petite maison",
-            smalldescription = "Petite maison à Sandy Shores",
-            description = "Une petite maison avec vue panoramique sur Sandy Shores, idéal pour commencer votre aventure dans le luxe et le confort.",
+            adress = "Sandy Shores trailer",
+            smalldescription = "Trailer home in Sandy Shores",
+            description = "Start out in the heart of the desert with quick access to Blaine County jobs and activities.",
             image = "https://img.gta5-mods.com/q75/images/sandy-shores-enhancement/912986-GTA5%202015-11-22%2017-16-49-49.jpg",
             top = 30,
             left = 37,
@@ -39,6 +50,6 @@ BOUTIQUE.Config = {
         }
     },
     translate = {
-        selectSpawn = 'Apparaître ici'
+        selectSpawn = 'Spawn Here'
     }
 }
