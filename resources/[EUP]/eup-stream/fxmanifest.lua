78077594e4177db0aaa9885332cee2af27fe6574
cstone1995 <xdreamzclan@gmail.com>
2025-11-20 19:24:49 +0000

    h

---
+++ resources/[EUP]/eup-stream/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'EUP'
+description 'EUP Stream Base'
+version '1.0.0'
+
+-- Empty resource, placeholder for EUP stream files if needed
+
--- resources/[EUP]/ndrp_es_eup_v3/fxmanifest.lua
+++ resources/[EUP]/ndrp_es_eup_v3/fxmanifest.lua
@@ -9,17 +9,14 @@ files {
     'mp_f_freemode_01_mp_f_ndrp_esg_eup_v3_shop.meta',
     'mp_m_freemode_01_mp_m_ndrp_esg_eup_v3_shop.meta',
     'pedalternatevariations.meta',
-    'first_person_alternates.meta'
+    'first_person_alternates.meta',
+    'stream/**/*.ydd',
+    'stream/**/*.ytd',
+    'stream/**/*.ymt'
 }
 
 data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_ndrp_esg_eup_v3_shop.meta'
 data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_ndrp_esg_eup_v3_shop.meta'
 data_file 'PED_ALTERNATE_VARIATIONS_FILE' 'pedalternatevariations.meta'
 data_file 'FIRST_PERSON_ALTERNATE_VARIATIONS_FILE' 'first_person_alternates.meta'
-
-files {
-    'stream/**/*.ydd',
-    'stream/**/*.ytd',
-    'stream/**/*.ymt'
-}
 
