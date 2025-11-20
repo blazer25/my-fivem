f96500782c9cd26a31a4091671e21357f8b7b6d6
DaddyDankz420 <chevyboi789@gmail.com>
2025-11-20 12:09:55 -0600

    mlo

---
+++ resources/[maps]/henhouse_milo/__resource.lua
@@ -0,0 +1,8 @@
+this_is_a_map 'yes'
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+files {
+    "interiorproxies.meta"
+}
+
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
