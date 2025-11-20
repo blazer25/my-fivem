5a47ddd052172bc18cccbad2c7a297df7e3fa42d
cstone1995 <xdreamzclan@gmail.com>
2025-11-20 13:59:38 +0000

    f

---
+++ resources/[EUP]/ef_clothing_dlc1_male/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+
+games   { 'gta5' }
+
+name      'Fury Clothing'
+
+author    'Fury'
+
+contact   'Fury#9424'
+
+version '2.0.0'
+
+description 'A Addon Cloth Pack for Online Servers'
+
+files {
+    'mp_m_freemode_01_mp_m_ctrp.meta'
+  }
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_ctrp.meta'
