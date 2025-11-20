d992ee3b3e0a837ee15956524d686628fc1eb28e
cstone1995 <xdreamzclan@gmail.com>
2025-11-20 20:27:49 +0000

    v

--- resources/[EUP]/Female-EUP/fxmanifest.lua
+++ resources/[EUP]/Female-EUP/fxmanifest.lua
@@ -4,12 +4,8 @@ game 'gta5'
 description 'Female EUP Clothing Pack'
 version '1.0.0'
 
-files {
-    'stream/**/*.ydd',
-    'stream/**/*.ytd',
-    'stream/**/*.ymt'
-}
-
-data_file 'COMPONENTPEDS_FILE' 'stream/**/player_one.ymt'
+-- This is a stream-only EUP pack
+-- Clothing items are added via stream files only
+-- No metadata files required for this type of pack
 
 lua54 'yes'
