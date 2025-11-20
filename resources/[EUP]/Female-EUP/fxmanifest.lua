ffcb104c26c99455e3b8567c888651beca6114f7
cstone1995 <xdreamzclan@gmail.com>
2025-11-20 21:31:03 +0000

    s

--- resources/[EUP]/Female-EUP/fxmanifest.lua
+++ resources/[EUP]/Female-EUP/fxmanifest.lua
@@ -4,12 +4,6 @@ game 'gta5'
 description 'Female EUP Clothing Pack'
 version '1.0.0'
 
-files {
-    'stream/**/*.ydd',
-    'stream/**/*.ytd',
-    'stream/**/*.ymt'
-}
-
 this_is_a_map 'yes'
 
 lua54 'yes'
