d9cda8e59a5a3a2243c525b19b795a72720ae7e9
cstone1995 <xdreamzclan@gmail.com>
2025-11-21 19:45:54 +0000

    d

---
+++ resources/[EUP]/Female-EUP/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Female EUP Clothing Pack'
+version '1.0.0'
+
+-- Stream-only resource for texture replacements
+-- No files section needed - textures stream automatically
+
+lua54 'yes'
