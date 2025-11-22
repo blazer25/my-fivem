4326ac5be504c128ad8aed85d5423da6fd9ff3b4
cstone1995 <xdreamzclan@gmail.com>
2025-11-22 02:23:44 +0000

    s

--- resources/[standalone]/illenium-appearance/server/framework/qb/main.lua
+++ resources/[standalone]/illenium-appearance/server/framework/qb/main.lua
@@ -1,8 +1,22 @@
 if not Framework.QBCore() then return end
 
-local QBCore = exports["qb-core"]:GetCoreObject()
+-- Try QBX Core first, then fall back to QB Core
+local QBCore
+if GetResourceState('qbx_core') == 'started' then
+    -- Use QBX Core
+    QBCore = exports.qbx_core
+elseif GetResourceState('qb-core') == 'started' then
+    -- Use QB Core
+    QBCore = exports["qb-core"]:GetCoreObject()
+else
+    -- No compatible framework found
+    print("^1[illenium-appearance] No compatible QB framework found on server!^7")
+    return
+end
 
 function Framework.GetPlayerID(src)
+    if not QBCore or not QBCore.Functions then return nil end
+    
     local Player = QBCore.Functions.GetPlayer(src)
     if Player then
         return Player.PlayerData.citizenid
@@ -10,23 +24,43 @@ function Framework.GetPlayerID(src)
 end
 
 function Framework.HasMoney(src, type, money)
+    if not QBCore or not QBCore.Functions then return false end
+    
     local Player = QBCore.Functions.GetPlayer(src)
-    return Player.PlayerData.money[type] >= money
+    if Player and Player.PlayerData and Player.PlayerData.money then
+        return Player.PlayerData.money[type] >= money
+    end
+    return false
 end
 
 function Framework.RemoveMoney(src, type, money)
+    if not QBCore or not QBCore.Functions then return false end
+    
     local Player = QBCore.Functions.GetPlayer(src)
-    return Player.Functions.RemoveMoney(type, money)
+    if Player and Player.Functions then
+        return Player.Functions.RemoveMoney(type, money)
+    end
+    return false
 end
 
 function Framework.GetJob(src)
+    if not QBCore or not QBCore.Functions then return nil end
+    
     local Player = QBCore.Functions.GetPlayer(src)
-    return Player.PlayerData.job
+    if Player and Player.PlayerData then
+        return Player.PlayerData.job
+    end
+    return nil
 end
 
 function Framework.GetGang(src)
+    if not QBCore or not QBCore.Functions then return nil end
+    
     local Player = QBCore.Functions.GetPlayer(src)
-    return Player.PlayerData.gang
+    if Player and Player.PlayerData then
+        return Player.PlayerData.gang
+    end
+    return nil
 end
 
 function Framework.SaveAppearance(appearance, citizenID)
