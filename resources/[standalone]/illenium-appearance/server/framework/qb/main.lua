c4b33a20ae9b2d3c48c9326094edb3166655eb25
cstone1995 <xdreamzclan@gmail.com>
2025-11-22 02:27:02 +0000

    d

--- resources/[standalone]/illenium-appearance/server/framework/qb/main.lua
+++ resources/[standalone]/illenium-appearance/server/framework/qb/main.lua
@@ -5,12 +5,20 @@ local QBCore
 if GetResourceState('qbx_core') == 'started' then
     -- Use QBX Core
     QBCore = exports.qbx_core
+    print("^2[illenium-appearance] Using QBX Core on server^7")
 elseif GetResourceState('qb-core') == 'started' then
     -- Use QB Core
     QBCore = exports["qb-core"]:GetCoreObject()
+    print("^2[illenium-appearance] Using QB Core on server^7")
 else
     -- No compatible framework found
     print("^1[illenium-appearance] No compatible QB framework found on server!^7")
+    return
+end
+
+-- Verify QBCore is properly loaded
+if not QBCore or not QBCore.Functions then
+    print("^1[illenium-appearance] QBCore functions not available on server!^7")
     return
 end
 
