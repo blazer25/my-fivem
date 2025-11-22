f7ea68889228b8676544849387603011aa8cbc04
cstone1995 <xdreamzclan@gmail.com>
2025-11-22 15:57:11 +0000

    d

--- resources/[standalone]/cardboard-riddles/client/cardboard.lua
+++ resources/[standalone]/cardboard-riddles/client/cardboard.lua
@@ -1,4 +1,7 @@
+print('[Cardboard-Riddles] Client script loaded')
+
 local function showCardboardImage(image)
+    print('[Cardboard] Showing image:', image)
     SendNUIMessage({
         action = 'showCardboard',
         image = image
@@ -6,14 +9,32 @@ local function showCardboardImage(image)
     SetNuiFocus(true, true)
 end
 
-RegisterNetEvent('cardboard:read', function()
-    print('[Cardboard] Event triggered - showing image')
-    -- Default to password image
-    -- You can modify this to check metadata if needed
-    showCardboardImage('backmarket-password.png')
+-- ox_inventory uses TriggerEvent (local event), not network event
+AddEventHandler('cardboard:read', function(data, itemData)
+    print('[Cardboard] Event triggered!')
+    if data then print('[Cardboard] Data received') end
+    if itemData then 
+        print('[Cardboard] ItemData:', itemData.name or 'no name')
+        if itemData.metadata then
+            print('[Cardboard] Metadata:', json.encode(itemData.metadata))
+        end
+    end
+    
+    -- Check metadata to determine which image to show
+    local metadata = itemData and itemData.metadata or {}
+    local imageType = metadata.type or 'password' -- default to password
+    
+    print('[Cardboard] Image type:', imageType)
+    
+    if imageType == 'location' then
+        showCardboardImage('backmarket-location.png')
+    else
+        showCardboardImage('backmarket-password.png')
+    end
 end)
 
 RegisterNUICallback('closeCardboard', function()
+    print('[Cardboard] Closing NUI')
     SetNuiFocus(false, false)
 end)
 
