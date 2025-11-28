73422ee1159634475efab0aa921b9e7aace4cc35
cstone1995 <xdreamzclan@gmail.com>
2025-11-28 17:30:30 +0000

    s

--- resources/[EUP]/Male-EUP/fxmanifest.lua
+++ resources/[EUP]/Male-EUP/fxmanifest.lua
@@ -14,6 +14,6 @@ files {
     'stream/mp_m_freemode_01_mp_m_ensart_sonsof.ymt'
 }
 
-data_file 'PED_ALTERNATE_VARIATIONS_FILE' 'pedalternatevariations.meta'
+--- data_file 'PED_ALTERNATE_VARIATIONS_FILE' 'pedalternatevariations.meta'
 data_file 'SHOP_PED_APPAREL_FILE' 'mp_f_freemode_01_mp_f_ensart_sonsof.meta'
 data_file 'SHOP_PED_APPAREL_FILE' 'mp_m_freemode_01_mp_m_ensart_sonsof.meta'
