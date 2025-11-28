baeaf5e6c1f30da9e30411d4e7ead11636a683ca
cstone1995 <xdreamzclan@gmail.com>
2025-11-28 18:42:33 +0000

    reupload of sons kit

---
+++ resources/[eup]/ensart_sonsof/fxmanifest.lua
@@ -0,0 +1,15 @@
+-- Generated with DurtyClothTool
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_ensart_sonsof.meta',
+  'mp_f_freemode_01_mp_f_ensart_sonsof.meta',
+  'pedalternatevariations.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_ensart_sonsof.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_ensart_sonsof.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'pedalternatevariations.meta'
+dependency '/assetpacks'
