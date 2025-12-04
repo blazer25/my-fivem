111c8016eeee05b07749e245f0c8bcdd77eefc2c
cstone1995 <xdreamzclan@gmail.com>
2025-12-04 23:31:54 +0000

    dev

---
+++ resources/[eup]/104S_TshirtPack/README.txt
@@ -0,0 +1,10 @@
+Textured by 104SHOP ! 
+Join discord for more !!
+
+https://discord.gg/Pn8YWatz
+https://discord.gg/Pn8YWatz
+https://discord.gg/Pn8YWatz
+https://discord.gg/Pn8YWatz
+https://discord.gg/Pn8YWatz
+
+ENJOY :) !
---
+++ resources/[eup]/104S_TshirtPack/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author '104SHOP'
+description '104S Tshirt Pack EUP'
+version '1.0.0'
+
+files {
+  'mp_m_freemode_01_104s_tshirtpack.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_104s_tshirtpack.meta'
+
+dependency '/assetpacks'
+
